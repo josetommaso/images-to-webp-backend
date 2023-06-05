1d188ac9854d5c8209c826265ac24f19cf54d0fb
Jose <tommaso_j@hotmail.com>
2023-06-05 16:01:10 +0200

    change port

--- server.js
+++ server.js
@@ -128,6 +128,6 @@ const zipWebPFiles = async (webpImages, uploadPath) => {
 };
 
 // Start the server
-app.listen(4000, () => {
-	console.log('Server is running on port 4000');
+app.listen(3000, () => {
+	console.log('Server is running on port 3000');
 });
